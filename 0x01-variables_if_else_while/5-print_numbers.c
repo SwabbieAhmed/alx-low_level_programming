c7ee819a5c3ed425f81d6167745f08865779b8f2
SwabbieAhmed <ahmedswabrina@yahoo.com>
2023-03-19 15:59:21 -0700

    Numbers 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all single digits form 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
