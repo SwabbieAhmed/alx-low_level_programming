392cbcb576e28239bda58fa69d54715be9185d11
SwabbieAhmed <ahmedswabrina@yahoo.com>
2023-03-28 05:21:02 -0700

    Additiong of endif in main header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -11,3 +11,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+
+#endif
