66ecfa74216720da901020683e9f952f1c482eaf
SwabbieAhmed <ahmedswabrina@yahoo.com>
2023-03-28 10:54:05 -0700

    3. I do not fear commputers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to a stdout, followed by new line
+ *
+ * @str: pointer to string
+ *
+ * Return: none
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		str++;
+	}
+	putchar('\n');
+}
