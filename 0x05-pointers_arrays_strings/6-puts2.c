5df6dd8d11a463a477d6fb6434feceb61d4fd2c0
SwabbieAhmed <ahmedswabrina@yahoo.com>
2023-03-28 13:06:21 -0700

    Correct output to code

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,10 +11,12 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		putchar(str[i]);
-		i += 2;
+		if ((i % 2) == 0)
+			putchar(str[i]);
+		else
+			continue;
 	}
 	putchar('\n');
 }
