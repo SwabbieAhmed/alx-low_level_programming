97a1f528261abc7264a625f57951e00dbb25e25f
SwabbieAhmed <ahmedswabrina@yahoo.com>
2023-03-29 10:51:58 -0700

    1. strncat Correction to code

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,27 +3,22 @@
 
 /**
  * _strncat - it concatenates two strings
- * @dest: the destination string to be appended
+ * @dest: the destination strng we are to append
  * @src: the source string we add to
  * @n: the number of bytes from src to concatenate
  * Return: the pointer to the resulting destination string
- */
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int dest_len = 0;
-        int i = 0;
+	int index = 0, dest_len = 0;
 
-        while (dest[dest_len] != '\0')
-        {
-                dest_len++;
-        }
+	while (dest[index++])
+		dest_len++;
 
-        while (src[i] != '\0' && i < n)
-        {
-                dest[dest_len + i] = src[i];
-                i++;
-        }
-        dest[dest_len + i] = '\0';
-        return (dest);
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
+
